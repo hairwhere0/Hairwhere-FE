412a35bfa14bd3538680b1ca0255961bb81dc739
san9901 <san990190@gmail.com>
2025-04-07 10:41:52 +0900

    feat: 백엔드 api 연결

--- src/app/(main)/_lib/getManPosts.ts
+++ src/app/(main)/_lib/getManPosts.ts
@@ -3,11 +3,12 @@ import axios from "axios";
 type Props = {pageParam?: number};
 
 export default async function getManPosts({pageParam}: Props) {
-  const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/photo/findByGender/male?page=${pageParam}&size=15`);
-
-  if(!res.data.code) {
-    throw new Error('Failed to fetch data');
+  try {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/photo/findByGender/male?page=${pageParam}&size=15`);
+    console.log('응답 데이터:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('에러 상세 정보:', error);
+    throw error;
   }
-
-  return res.data.result;
 };
