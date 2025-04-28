4a796ba3010bf9977ac4687efd857646d44bd430
san9901 <san990190@gmail.com>
2025-04-28 12:21:15 +0900

    feat: 로그아웃 기능 추가

--- src/app/(main)/_component/LoginButton.tsx
+++ src/app/(main)/_component/LoginButton.tsx
@@ -5,10 +5,20 @@ import { useRouter } from 'next/navigation';
 
 export default function LoginButton() {
   const router = useRouter();
+  const accessToken = localStorage.getItem('accessToken');
+
+  const onLogout = () => {
+    localStorage.removeItem('accessToken');
+    document.cookie = `accessToken=; path=/; max-age=0`;
+    router.replace('/');
+  };
 
   return (
     <>
-      <div className={style.loginButton} onClick={() => router.push('/login')}>로그인</div>
+    {accessToken ?
+      <div className={style.loginButton} onClick={onLogout}>로그아웃</div>
+      : <div className={style.loginButton} onClick={() => router.push('/login')}>로그인</div>
+    }
     </>
   )
 }
